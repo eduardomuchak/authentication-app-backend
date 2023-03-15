14a8be946530102c7b9ecfe1832a9e75c40897a1
G-PPeralta <prates_peralta@hotmail.com>
2023-03-15 12:02:04 -0300

    feat: implements validate function on auth service and local strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(id: number, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(id);
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(pass, salt);
+
+    if (user && bcrypt.compare(pass, hash)) {
+      return user;
+    }
+    return null;
+  }
+}
